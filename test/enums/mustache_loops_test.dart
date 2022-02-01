83e3bbb3cbcbdbc9234e2f9f8e612d4bfaa942ed
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-01 17:37:35 -0600

    feat: add mustache loops test

---
+++ test/enums/mustache_loops_test.dart
@@ -0,0 +1,50 @@
+import 'package:brick_oven/enums/mustache_loops.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('$MustacheLoops', () {
+    test('#start returns the correct value', () {
+      expect(MustacheLoops.start, 'start');
+    });
+
+    test('#end returns the correct value', () {
+      expect(MustacheLoops.end, 'end');
+    });
+
+    test('#startInvert returns the correct value', () {
+      expect(MustacheLoops.startInvert, 'nstart');
+    });
+
+    group('#toMustache', () {
+      const key = 'purple';
+
+      test('returns the correct value for start', () {
+        expect(
+          MustacheLoops.toMustache(key, () => MustacheLoops.start),
+          '{{#$key}}',
+        );
+      });
+
+      test('returns the correct value for end', () {
+        expect(
+          MustacheLoops.toMustache(key, () => MustacheLoops.startInvert),
+          '{{^$key}}',
+        );
+      });
+
+      test('returns the correct value for startInvert', () {
+        expect(
+          MustacheLoops.toMustache(key, () => MustacheLoops.end),
+          '{{/$key}}',
+        );
+      });
+
+      test('throws when given an invalid value', () {
+        expect(
+          () => MustacheLoops.toMustache(key, () => 'invalid'),
+          throwsA(isA<ArgumentError>()),
+        );
+      });
+    });
+  });
+}
