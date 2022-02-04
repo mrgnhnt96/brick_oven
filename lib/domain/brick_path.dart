e8108ace88564460e674f78f6aa304e34cbb7aab
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-04 08:27:08 -0600

    feat: add methods to clean & separate path, throw exception if path is pointed to file

--- lib/domain/brick_path.dart
+++ lib/domain/brick_path.dart
@@ -1,3 +1,4 @@
+import 'package:brick_oven/domain/yaml_value.dart';
 import 'package:brick_oven/enums/mustache_format.dart';
 import 'package:equatable/equatable.dart';
 import 'package:path/path.dart';
@@ -8,12 +9,13 @@ class BrickPath extends Equatable {
     required String name,
     required String path,
   }) {
-    final _path = path.replaceAll(slashPattern, '');
+    final _path = cleanPath(path);
 
     return BrickPath._(
       name: name,
+      originalPath: path,
       path: _path,
-      placeholder: _path.substring(_path.lastIndexOf(separator) + 1),
+      placeholder: basename(_path),
     );
   }
 
@@ -21,29 +23,40 @@ class BrickPath extends Equatable {
     required this.name,
     required this.path,
     required this.placeholder,
+    required this.originalPath,
   });
 
-  factory BrickPath.fromYaml(String path, dynamic yaml) {
+  factory BrickPath.fromYaml(String path, YamlValue yaml) {
+    if (extension(path).isNotEmpty) {
+      throw ArgumentError.value(
+        path,
+        'path',
+        'Path must not have an extension',
+      );
+    }
+
     String? handleYaml(YamlMap yaml) {
       final data = yaml.value;
 
       final name = data.remove('name') as String?;
 
       if (data.isNotEmpty) {
-        throw ArgumentError('Unknown keys in path: ${data.keys}');
+        throw ArgumentError.value(
+          data.keys,
+          'Unknown keys',
+          'Remove all unknown keys from path $path',
+        );
       }
 
       return name;
     }
 
     String? name;
-    switch (yaml.runtimeType) {
-      case String:
-        name = yaml as String;
-        break;
-      case YamlMap:
-        name = handleYaml(yaml as YamlMap);
-        break;
+
+    if (yaml.isYaml()) {
+      name = handleYaml(yaml.asYaml().value);
+    } else if (yaml.isString()) {
+      name = yaml.asString().value;
     }
 
     name ??= basename(path);
@@ -54,27 +67,40 @@ class BrickPath extends Equatable {
   final String placeholder;
   final String name;
   final String path;
+  final String originalPath;
 
-  static RegExp separatorPattern = RegExp(r'(?<=[\w|}])\' + separator);
-  static RegExp slashPattern =
-      RegExp(r'^\' + separator + r'|\' + separator + r'$');
+  static RegExp separatorPattern = RegExp(r'(?<=[\w|}])[\/\\]');
+  static RegExp slashPattern = RegExp(r'^[\/\\]+|[\/\\]+$');
 
-  List<String> get configuredParts => path.split(separatorPattern);
+  static List<String> separatePath(String path) {
+    final pathParts = cleanPath(path).split(RegExp(r'[\/\\]'))
+      ..removeWhere((part) => part.isEmpty);
+
+    return pathParts;
+  }
+
+  static String cleanPath(String path) {
+    final normalPath = normalize(path);
+    final cleanPath = normalPath.replaceAll(slashPattern, '');
+
+    return cleanPath;
+  }
+
+  List<String> get configuredParts => separatePath(path);
 
   String apply(
     String path, {
     required String originalPath,
   }) {
-    final isNotFile = extension(placeholder).isNotEmpty;
     final isNotDirectoryDeep = !this.path.contains(separatorPattern);
     final pathsDontMatch = !originalPath.contains(this.path);
 
-    if (isNotFile || isNotDirectoryDeep || pathsDontMatch) {
+    if (isNotDirectoryDeep || pathsDontMatch) {
       return path;
     }
 
     // ignore: parameter_assignments
-    path = path.replaceAll(slashPattern, '');
+    path = normalize(path).replaceAll(slashPattern, '');
 
     final replacement = MustacheFormat.snakeCase.toMustache('{{{$name}}}');
 
