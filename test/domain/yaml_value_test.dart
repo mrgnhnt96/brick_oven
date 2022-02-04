43987d24a4a92ded2bb340c1f2f6ba3c6899611e
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-04 07:16:35 -0600

    feat: add tests for yaml value

---
+++ test/domain/yaml_value_test.dart
@@ -0,0 +1,214 @@
+// ignore_for_file: prefer_void_to_null
+
+import 'package:brick_oven/domain/yaml_value.dart';
+import 'package:test/test.dart';
+
+import '../utils/fakes.dart';
+
+void main() {
+  test('#string can be instanciated', () {
+    const string = YamlValue.string('test');
+
+    expect(string, isA<YamlValue>());
+    expect(string, isA<YamlString>());
+  });
+
+  test('#yaml can be instanciated', () {
+    final yaml = YamlValue.yaml(FakeYamlMap(<String, dynamic>{}));
+
+    expect(yaml, isA<YamlValue>());
+    expect(yaml, isA<YamlMapValue>());
+  });
+
+  test('#none can be instanciated', () {
+    const none = YamlValue.none();
+
+    expect(none, isA<YamlValue>());
+    expect(none, isA<YamlNone>());
+  });
+
+  group('#from', () {
+    test('can receive a string', () {
+      const data = 'test';
+      final value = YamlValue.from(data);
+
+      expect(value, isA<YamlValue>());
+      expect(value, isA<YamlString>());
+      expect(value.value, data);
+    });
+
+    test('can receive a yaml', () {
+      final data = FakeYamlMap(<String, dynamic>{'test': 'test'});
+      final value = YamlValue.from(data);
+
+      expect(value, isA<YamlValue>());
+      expect(value, isA<YamlMapValue>());
+      expect(value.value, data);
+    });
+
+    test('can receive a null', () {
+      const Null data = null;
+      final value = YamlValue.from(data);
+
+      expect(value, isA<YamlValue>());
+      expect(value, isA<YamlNone>());
+      expect(value.value, data);
+    });
+
+    test('should throw when provided non-handled type', () {
+      final data = <String, dynamic>{};
+
+      expect(() => YamlValue.from(data), throwsA(isA<UnsupportedError>()));
+    });
+  });
+
+  group('#isString', () {
+    test('returns true when provided a string', () {
+      final value = YamlValue.from('test');
+
+      expect(value.isString(), isTrue);
+    });
+
+    test('returns false when provided a non-string', () {
+      final value = YamlValue.from(FakeYamlMap(<String, dynamic>{}));
+
+      expect(value.isString(), isFalse);
+    });
+  });
+
+  group('#isYaml', () {
+    test('returns true when provided a yaml', () {
+      final value = YamlValue.from(FakeYamlMap(<String, dynamic>{}));
+
+      expect(value.isYaml(), isTrue);
+    });
+
+    test('returns false when provided a non-yaml', () {
+      final value = YamlValue.from('test');
+
+      expect(value.isYaml(), isFalse);
+    });
+  });
+
+  group('#isNone', () {
+    test('returns true when provided a null', () {
+      final value = YamlValue.from(null);
+
+      expect(value.isNone(), isTrue);
+    });
+
+    test('returns false when provided a value', () {
+      final value = YamlValue.from('test');
+
+      expect(value.isNone(), isFalse);
+    });
+  });
+
+  group('#asString', () {
+    test('should return as $YamlString when value is string', () {
+      final value = YamlValue.from('test');
+
+      expect(value.asString(), isA<YamlString>());
+    });
+
+    test('should throw when value is not string', () {
+      final value = YamlValue.from(FakeYamlMap(<String, dynamic>{}));
+
+      expect(value.asString, throwsA(isA<ArgumentError>()));
+    });
+  });
+
+  group('#asYaml', () {
+    test('should return as $YamlMapValue when value is yaml', () {
+      final value = YamlValue.from(FakeYamlMap(<String, dynamic>{}));
+
+      expect(value.asYaml(), isA<YamlMapValue>());
+    });
+
+    test('should throw when value is not yaml', () {
+      final value = YamlValue.from('test');
+
+      expect(value.asYaml, throwsA(isA<ArgumentError>()));
+    });
+  });
+
+  group('#asNone', () {
+    test('should return as $YamlNone when value is null', () {
+      final value = YamlValue.from(null);
+
+      expect(value.asNone(), isA<YamlNone>());
+    });
+
+    test('should throw when value is not null', () {
+      final value = YamlValue.from('test');
+
+      expect(value.asNone, throwsA(isA<ArgumentError>()));
+    });
+  });
+
+  group('$YamlString', () {
+    test('can be instanciated', () {
+      const string = YamlString('test');
+
+      expect(string, isA<YamlString>());
+    });
+
+    test('is type $YamlValue', () {
+      const string = YamlString('test');
+
+      expect(string, isA<YamlValue>());
+    });
+
+    test('value is type String', () {
+      const string = YamlString('test');
+
+      expect(string.value, isA<String>());
+    });
+  });
+
+  group('$YamlMapValue', () {
+    test('can be instanciated', () {
+      final yaml = FakeYamlMap(<String, dynamic>{});
+
+      final value = YamlMapValue(yaml);
+
+      expect(value, isA<YamlMapValue>());
+    });
+
+    test('is type $YamlValue', () {
+      final yaml = FakeYamlMap(<String, dynamic>{});
+
+      final value = YamlMapValue(yaml);
+
+      expect(value, isA<YamlValue>());
+    });
+
+    test('value is type YamlMap', () {
+      final yaml = FakeYamlMap(<String, dynamic>{});
+
+      final value = YamlMapValue(yaml);
+
+      expect(value.value, isA<FakeYamlMap>());
+    });
+  });
+
+  group('$YamlNone', () {
+    test('can be instanciated', () {
+      const none = YamlNone();
+
+      expect(none, isA<YamlNone>());
+    });
+
+    test('is type $YamlValue', () {
+      const none = YamlNone();
+
+      expect(none, isA<YamlValue>());
+    });
+
+    test('value is type null', () {
+      const none = YamlNone();
+
+      expect(none.value, isA<Null>());
+    });
+  });
+}
