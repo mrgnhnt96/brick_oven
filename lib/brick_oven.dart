7e50529925bf529a24ece9b2497b6f32c62abf87
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-05 19:18:19 -0600

    refactor: rename method to runBrickOven, add arguments type,

--- lib/brick_oven.dart
+++ lib/brick_oven.dart
@@ -1,5 +1,9 @@
+import 'package:brick_oven/domain/brick_arguments.dart';
 import 'package:brick_oven/domain/brick_config.dart';
 
-void main(List<String> arguments) {
-  BrickConfig().writeMason();
+export 'package:brick_oven/domain/brick_arguments.dart';
+
+/// runs the brick oven, generating bricks
+Future<void> runBrickOven(BrickArguments arguments) async {
+  await BrickConfig(arguments).writeMason();
 }
