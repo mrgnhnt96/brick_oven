2d9fb68ab2cf057d990ad4f1c9f010dc3a4edfff
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-08 17:11:48 -1000

    feat: handle errorCode 205, rebuild

--- lib/brick_oven.dart
+++ lib/brick_oven.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:brick_oven/domain/brick_arguments.dart';
 import 'package:brick_oven/domain/brick_config.dart';
 
@@ -6,4 +8,10 @@ export 'package:brick_oven/domain/brick_arguments.dart';
 /// runs the brick oven, generating bricks
 Future<void> runBrickOven(BrickArguments arguments) async {
   await BrickConfig(arguments).writeMason();
+
+  if (exitCode == 205) {
+    await runBrickOven(arguments);
+  }
+
+  exit(0);
 }
