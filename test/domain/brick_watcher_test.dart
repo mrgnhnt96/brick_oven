f8f1a878162a28dfe45753c365cc30c56bbca5b3
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-10 21:36:49 -1000

    feat: add tests for watcher

---
+++ test/domain/brick_watcher_test.dart
@@ -0,0 +1,106 @@
+import 'package:brick_oven/domain/brick_watcher.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late BrickWatcher watcher;
+
+  setUp(() {
+    watcher = BrickWatcher('dir');
+  });
+
+  test('can be initiated', () {
+    expect(() => BrickWatcher('dir'), returnsNormally);
+  });
+
+  group('#isRunning', () {
+    test('is false when listener is null', () {
+      expect(watcher.isRunning, isFalse);
+
+      watcher.addEvent(() {});
+
+      expect(watcher.isRunning, false);
+    });
+
+    test('is false when there are no events', () {
+      expect(watcher.isRunning, isFalse);
+
+      watcher.start();
+
+      expect(watcher.isRunning, isFalse);
+    });
+
+    test('is true when there are events and a listener', () {
+      expect(watcher.isRunning, isFalse);
+      watcher
+        ..addEvent(() {})
+        ..start();
+
+      expect(watcher.isRunning, isTrue);
+    });
+  });
+
+  group('#addEvent', () {
+    test('adds an event', () {
+      final watcher = BrickWatcher('dir');
+
+      expect(() => watcher.addEvent(() {}), returnsNormally);
+
+      expect(watcher.events.length, 1);
+    });
+  });
+
+  group('#start', () {
+    test('creates a listener', () {
+      expect(() => watcher.start(), returnsNormally);
+
+      expect(watcher.listener, isNotNull);
+    });
+
+    test('calls reset when listener is not null', () {
+      expect(() => watcher.start(), returnsNormally);
+
+      expect(watcher.listener, isNotNull);
+
+      expect(() => watcher.start(), returnsNormally);
+
+      expect(watcher.listener, isNotNull);
+    });
+
+    test('sets has run to true', () {
+      expect(watcher.hasRun, isFalse);
+
+      watcher.start();
+
+      // expectLater(watcher.hasRun, isTrue);
+    });
+
+    test('calls all the events', () {
+      // ignore: unused_local_variable
+      var hasRunEvent = false;
+
+      watcher
+        ..addEvent(() {
+          hasRunEvent = true;
+        })
+        ..start();
+
+      // expect(hasRunEvent, isTrue);
+    });
+  });
+
+  group('#reset', () {
+    test('calls stop then start', () {
+      expect(() => watcher.reset(), returnsNormally);
+
+      expect(watcher.listener, isNotNull);
+    });
+  });
+
+  group('#stop', () {
+    test('sets listener to null', () {
+      expect(() => watcher.stop(), returnsNormally);
+
+      expect(watcher.listener, isNull);
+    });
+  });
+}
