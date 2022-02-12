31dffcd731f5cc017bc1c1878894c2ac5a10ddf0
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-12 01:59:29 -1000

    feat: pass arguments, handle flush & exit

--- lib/brick_oven.dart
+++ lib/brick_oven.dart
@@ -1,17 +1,30 @@
 import 'dart:io';
 
-import 'package:brick_oven/domain/brick_arguments.dart';
-import 'package:brick_oven/domain/brick_config.dart';
-
-export 'package:brick_oven/domain/brick_arguments.dart';
+import 'package:brick_oven/src/runner.dart';
+import 'package:mason_logger/mason_logger.dart';
+import 'package:pub_updater/pub_updater.dart';
 
 /// runs the brick oven, generating bricks
-Future<void> runBrickOven(BrickArguments arguments) async {
-  await BrickConfig(arguments).writeBricks();
+Future<void> runBrickOven(List<String> arguments) async {
+  final exitCode = await BrickOvenRunner(
+    logger: Logger(),
+    pubUpdater: PubUpdater(),
+  ).run(arguments);
 
-  if (exitCode == 205) {
+  if (exitCode == ExitCode.tempFail.code) {
     await runBrickOven(arguments);
   }
 
-  exit(0);
+  await flushThenExit(ExitCode.success.code);
+}
+
+/// Flushes the stdout and stderr streams, then exits the program with the given
+/// status code.
+///
+/// This returns a Future that will never complete, since the program will have
+/// exited already. This is useful to prevent Future chains from proceeding
+/// after you've decided to exit.
+Future flushThenExit(int status) {
+  return Future.wait<void>([stdout.close(), stderr.close()])
+      .then<void>((_) => exit(status));
 }
