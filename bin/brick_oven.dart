9f444ce8136a33b9f247abaa46ab8aac70faf4c4
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-12 01:58:45 -1000

    refactor: remove args parse

--- bin/brick_oven.dart
+++ bin/brick_oven.dart
@@ -1,7 +1,5 @@
 import 'package:brick_oven/brick_oven.dart';
 
 void main(List<String> arguments) {
-  final args = BrickArguments.from(arguments);
-
-  runBrickOven(args);
+  runBrickOven(arguments);
 }
