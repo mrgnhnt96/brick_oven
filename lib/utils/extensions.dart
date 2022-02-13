aea4befca3cef9bae84e6f1746987ae5252f0a5d
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-12 16:33:36 -1000

    feat: add extension for cooking and watching files

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,3 +1,4 @@
+import 'package:mason_logger/mason_logger.dart';
 import 'package:yaml/yaml.dart';
 
 /// the extension for [YamlMap]
@@ -6,5 +7,18 @@ extension YamlMapX on YamlMap {
   /// returns a modifiable Map of the[value]
   Map<String, dynamic> get data {
     return Map<String, dynamic>.from(value);
+  }
+}
+
+/// the extension for [Logger]
+extension LoggerX on Logger {
+  /// writes `Cooking...`
+  void cooking() {
+    info(cyan.wrap('\nCooking...'));
+  }
+
+  /// writes `Watching local files...`
+  void watching() {
+    info(lightYellow.wrap('\nWatching local files...'));
   }
 }
