24ad3e608678b3264948b2612d172d5f0050a1b5
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-17 18:57:58 -1000

    feat: add exceptions

---
+++ test/src/exception_test.dart
@@ -0,0 +1,36 @@
+import 'package:brick_oven/domain/brick_oven_yaml.dart';
+import 'package:brick_oven/src/exception.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('$BrickOvenException can be instanciated', () {
+    expect(() => const BrickOvenException('test'), returnsNormally);
+  });
+
+  test('$BrickOvenNotFoundException has the correct message', () {
+    expect(
+      const BrickOvenNotFoundException().message,
+      'Cannot find ${BrickOvenYaml.file}.'
+      '\nDid you forget to run brick_oven init?',
+    );
+  });
+
+  test('$BrickNotFoundException has the correct message', () {
+    const brick = 'test';
+    expect(
+      const BrickNotFoundException(brick).message,
+      'Cannot find $brick.\n'
+      'Make sure to provide a valid brick name '
+      'from the ${BrickOvenYaml.file}.',
+    );
+  });
+
+  test('$UnknownKeysException has the correct message', () {
+    const keys = ['test', 'test2'];
+    const location = 'location';
+    expect(
+      UnknownKeysException(keys, location).message,
+      'Unknown keys: ${keys.join(', ')}, in $location',
+    );
+  });
+}
