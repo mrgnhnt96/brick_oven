f2e18969598e1d1ab484d5cb1a1397ff7c96c3e2
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-17 20:57:40 -1000

    refactor: add skip

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -2,7 +2,11 @@ import 'package:brick_oven_test/brick_oven_test.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('runs', () async {
-    await testBrickOven();
-  });
+  test(
+    'runs',
+    () async {
+      await testBrickOven();
+    },
+    skip: true,
+  );
 }
