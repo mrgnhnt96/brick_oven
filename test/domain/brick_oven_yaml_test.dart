03a95ae55d33c4a40a7f90afcb05b2717558473e
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-18 21:30:44 -1000

    feat: create tests for brick oven yaml

---
+++ test/domain/brick_oven_yaml_test.dart
@@ -0,0 +1,64 @@
+import 'package:brick_oven/domain/brick_oven_yaml.dart';
+import 'package:file/file.dart';
+import 'package:path/path.dart';
+import 'package:test/test.dart';
+
+import '../utils/testing_env.dart';
+
+void main() {
+  var fs = setUpTestingEnvironment();
+  late File configFile;
+
+  setUp(() async {
+    fs = setUpTestingEnvironment();
+
+    final configPath = join(fs.currentDirectory.path, BrickOvenYaml.file);
+
+    configFile = fs.file(configPath);
+
+    await configFile.create(recursive: true);
+  });
+
+  tearDown(() {
+    tearDownTestingEnvironment(fs);
+  });
+
+  group('$BrickOvenYaml', () {
+    test('#file is brick_oven.yaml', () {
+      expect(BrickOvenYaml.file, 'brick_oven.yaml');
+    });
+
+    test('#findNearest finds the brick_oven file', () {
+      Directory directory(List<String> segments) {
+        return fs.directory(joinAll([fs.currentDirectory.path, ...segments]));
+      }
+
+      final dirs = [
+        directory(['some']),
+        directory(['some', 'other']),
+        directory(['some', 'other', 'path']),
+      ];
+
+      for (final dir in dirs) {
+        final brickOvenYaml = BrickOvenYaml.findNearest(dir);
+
+        expect(brickOvenYaml?.path, configFile.path);
+      }
+    });
+
+    test('#overWatcher runs normally', () {});
+
+    test('#watchForChanges should return true when file changes', () async {
+      var hasChanged = false;
+
+      final listener =
+          BrickOvenYaml.watchForChanges(onChange: () => hasChanged = true);
+
+      configFile.writeAsStringSync('update');
+
+      await listener;
+
+      expect(hasChanged, isTrue);
+    });
+  });
+}
