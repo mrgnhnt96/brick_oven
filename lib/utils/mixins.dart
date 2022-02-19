f1884ce83e9938d70e58ff78aca988f79e87c2b7
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-19 09:21:36 -1000

    feat: create mixin for quit after

---
+++ lib/utils/mixins.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:mason_logger/mason_logger.dart';
+
+/// the variables for quit after flag
+mixin QuitAfterMixin on Command<int> {
+  int _updates = 0;
+
+  /// the count of file updates have occurred
+  int get updates => _updates;
+
+  /// increments [updates] by 1
+  void fileChanged({Logger? logger}) {
+    _updates++;
+    if (shouldQuit) {
+      logger?.info('Quitting after $updates updates');
+      exit(ExitCode.success.code);
+    }
+  }
+
+  /// the number of updates that must occur before the command exits
+  ///
+  /// `null` means that the command will not exit
+  int? get quitAfter =>
+      _quitAfterArg == null ? null : int.parse(_quitAfterArg!);
+
+  String? get _quitAfterArg => argResults?['quit-after'] as String?;
+
+  /// whether the command should exit based on [updates]
+  bool get shouldQuit => quitAfter != null && updates >= quitAfter!;
+}
