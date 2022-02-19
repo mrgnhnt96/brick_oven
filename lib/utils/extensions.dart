dca1d532ccd0cb6385c381d2ef6f01c0b40aa0bf
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-19 09:17:23 -1000

    feat: add ext for q to quit, and general arg parser ext

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,3 +1,4 @@
+import 'package:args/args.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:yaml/yaml.dart';
 
@@ -20,5 +21,45 @@ extension LoggerX on Logger {
   /// writes `Watching local files...`
   void watching() {
     info(lightYellow.wrap('\nWatching local files...'));
+  }
+
+  /// writes `Press q to quit...`
+  void qToQuit() {
+    info(darkGray.wrap('Press q to quit...'));
+  }
+}
+
+/// extensions for [ArgParser]
+extension ArgParserX on ArgParser {
+  /// the output directory
+  void output() {
+    addOption(
+      'output',
+      abbr: 'o',
+      help: 'Sets the output directory',
+      valueHelp: 'path',
+      defaultsTo: 'bricks',
+    );
+  }
+
+  /// watches the local source files
+  void watch() {
+    addFlag(
+      'watch',
+      abbr: 'w',
+      negatable: false,
+      help: 'Watch the configuration file for changes and '
+          're-cook the bricks as they change.',
+    );
+  }
+
+  /// quits after x file updates
+  void quitAfter() {
+    addOption(
+      'quit-after',
+      abbr: 'x',
+      help: 'Quit after the specified number of updates.',
+      hide: true,
+    );
   }
 }
