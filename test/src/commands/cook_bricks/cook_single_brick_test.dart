12157ae854c695af0896d1dc4d9a6385964a6cd8
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-18 21:31:28 -1000

    feat: add tests for watch and outputDir

--- test/src/commands/cook_bricks/cook_single_brick_test.dart
+++ test/src/commands/cook_bricks/cook_single_brick_test.dart
@@ -1,10 +1,13 @@
+import 'package:args/args.dart';
 import 'package:brick_oven/domain/brick.dart';
 import 'package:brick_oven/domain/brick_oven_yaml.dart';
 import 'package:brick_oven/domain/brick_source.dart';
-import 'package:brick_oven/src/commands/cook_bricks/cook_bricks.dart';
+import 'package:brick_oven/src/commands/cook_bricks/cook_single_brick.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
 import 'package:test/test.dart';
+
+import '../../../utils/fakes.dart';
 
 void main() {
   late FileSystem fs;
@@ -52,5 +55,48 @@ bricks:
     test('name is cook', () {
       expect(brickOvenCommand.name, brick.name);
     });
+
+    group('#isWatch', () {
+      test('returns true when watch flag is provided', () {
+        final command =
+            TestCookSingleBrick(argResults: <String, dynamic>{'watch': true});
+
+        expect(command.isWatch, isTrue);
+      });
+
+      test('returns false when watch flag is not provided', () {
+        final command =
+            TestCookSingleBrick(argResults: <String, dynamic>{'watch': false});
+
+        expect(command.isWatch, isFalse);
+      });
+    });
+
+    group('#outputDir', () {
+      test('returns the output dir when provided', () {
+        final command = TestCookSingleBrick(
+          argResults: <String, dynamic>{'output': 'output/dir'},
+        );
+
+        expect(command.outputDir, 'output/dir');
+      });
+
+      test('returns null when not provided', () {
+        final command = TestCookSingleBrick(argResults: <String, dynamic>{});
+
+        expect(command.outputDir, 'bricks');
+      });
+    });
   });
+}
+
+class TestCookSingleBrick extends CookSingleBrick {
+  TestCookSingleBrick({required Map<String, dynamic> argResults})
+      : _argResults = argResults,
+        super(FakeBrick());
+
+  final Map<String, dynamic> _argResults;
+
+  @override
+  ArgResults get argResults => FakeArgResults(data: _argResults);
 }
