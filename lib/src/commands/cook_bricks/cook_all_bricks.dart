01e9dd87dcd9ec965cc0101d8cb4035577421176
Morgan Hunt <mrgnhnt96@gmail.com>
2022-02-21 21:14:21 -1000

    feat: pass config watcher as param, add config watcher mixin and organize print statements

--- lib/src/commands/cook_bricks/cook_all_bricks.dart
+++ lib/src/commands/cook_bricks/cook_all_bricks.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: overridden_fields
+
 import 'dart:async';
 import 'dart:io';
 
@@ -9,20 +11,27 @@ import 'package:brick_oven/utils/extensions.dart';
 import 'package:brick_oven/utils/mixins.dart';
 import 'package:file/file.dart';
 import 'package:mason_logger/mason_logger.dart';
+import 'package:watcher/watcher.dart';
 
 /// {@template cook_all_bricks_command}
 /// Writes all bricks from the configuration file
 /// {@endtemplate}
-class CookAllBricks extends BrickOvenCommand with QuitAfterMixin {
+class CookAllBricks extends BrickOvenCommand
+    with QuitAfterMixin, ConfigWatcherMixin {
   /// {@macro cook_all_bricks_command}
   CookAllBricks({
     FileSystem? fileSystem,
     Logger? logger,
-  }) : super(fileSystem: fileSystem, logger: logger) {
+    FileWatcher? configWatcher,
+  })  : configWatcher = configWatcher ?? FileWatcher(BrickOvenYaml.file),
+        super(fileSystem: fileSystem, logger: logger) {
     argParser
       ..addFlagsAndOptions()
       ..addSeparator('${'-' * 79}\n');
   }
+
+  @override
+  final FileWatcher configWatcher;
 
   @override
   String get description => 'Cook all bricks.';
@@ -52,18 +61,20 @@ class CookAllBricks extends BrickOvenCommand with QuitAfterMixin {
     }
 
     for (final brick in bricks) {
+      final watcher = brick.source.watcher;
+
       if (bricks.first == brick) {
-        brick.source.watcher?.addEvent(logger.cooking, runBefore: true);
-      } else if (bricks.last == brick) {
-        brick.source.watcher?.addEvent(logger.watching, runAfter: true);
-        brick.source.watcher?.addEvent(logger.qToQuit, runAfter: true);
+        watcher?.addEvent(logger.cooking, runBefore: true);
       }
 
-      brick.source.watcher?.addEvent(() => fileChanged(logger: logger));
+      if (bricks.last == brick) {
+        watcher?.addEvent(logger.watching, runAfter: true);
+        watcher?.addEvent(logger.qToQuit, runAfter: true);
+      }
+
+      watcher?.addEvent(() => fileChanged(logger: logger));
       brick.cook(output: outputDir, watch: true);
     }
-
-    logger.watching();
 
     if (!bricks.any((brick) => brick.source.watcher?.isRunning ?? false)) {
       logger.err(
@@ -73,11 +84,13 @@ class CookAllBricks extends BrickOvenCommand with QuitAfterMixin {
       return ExitCode.ioError.code;
     }
 
+    logger.watching();
+
     if (stdin.hasTerminal) {
       qToQuit(logger: logger);
     }
 
-    final ovenNeedsReset = await BrickOvenYaml.watchForChanges(
+    final ovenNeedsReset = await watchForConfigChanges(
       onChange: () {
         logger.alert(
           '${BrickOvenYaml.file} changed, updating bricks configuration',
