355ec944ec1143458e7156d274f3f340fc202f15
Morgan <mrgnhnt96@gmail.com>
2022-11-01 23:00:13 -0500

    feat: create brick or error

---
+++ lib/domain/brick_or_error.dart
@@ -0,0 +1,25 @@
+import 'package:brick_oven/domain/brick.dart';
+
+/// returns the bricks or the error that occurred
+/// when parsing the configuration file
+class BrickOrError {
+  /// returns the bricks or the error that occurred
+  /// when parsing the configuration file
+  const BrickOrError(this._bricks, this._error);
+
+  final Set<Brick>? _bricks;
+
+  /// the bricks of the configuration file
+  Set<Brick> get bricks => _bricks!;
+
+  final String? _error;
+
+  /// the error that occurred while parsing the configuration file
+  String get error => _error!;
+
+  /// if there was an error parsing the configuration file
+  bool get isError => _error != null;
+
+  /// if the parsing the configuration file was successfull
+  bool get isBricks => _bricks != null;
+}
