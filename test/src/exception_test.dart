e035aaf0ae0ba20b7227a4beea7aba3afde75fab
Morgan <mrgnhnt96@gmail.com>
2022-11-02 08:57:04 -0500

    feat: add tests for exceptions

--- test/src/exception_test.dart
+++ test/src/exception_test.dart
@@ -59,6 +59,126 @@ void main() {
     });
   });
 
+  group('$VariableException', () {
+    test('can be instanciated', () {
+      expect(
+        const VariableException(variable: 'test', reason: 'test'),
+        isA<BrickOvenException>(),
+      );
+
+      expect(
+        () => const VariableException(variable: 'test', reason: 'test'),
+        returnsNormally,
+      );
+    });
+
+    test('has the correct message', () {
+      const variable = '💩';
+      const reason = '🚽';
+
+      expect(
+        const VariableException(variable: variable, reason: reason).message,
+        'Variable "$variable" is invalid -- $reason',
+      );
+    });
+  });
+
+  group('$DirectoryException', () {
+    test('can be instanciated', () {
+      expect(
+        const DirectoryException(directory: 'test', reason: 'test'),
+        isA<BrickOvenException>(),
+      );
+
+      expect(
+        () => const DirectoryException(directory: 'test', reason: 'test'),
+        returnsNormally,
+      );
+    });
+
+    test('has the correct message', () {
+      const directory = '💩';
+      const reason = '🚽';
+
+      expect(
+        const DirectoryException(directory: directory, reason: reason).message,
+        'Directory "$directory" is invalid -- $reason',
+      );
+    });
+  });
+
+  group('$SourceException', () {
+    test('can be instanciated', () {
+      expect(
+        const SourceException(source: 'test', reason: 'test'),
+        isA<BrickOvenException>(),
+      );
+
+      expect(
+        () => const SourceException(source: 'test', reason: 'test'),
+        returnsNormally,
+      );
+    });
+
+    test('has the correct message', () {
+      const source = '💩';
+      const reason = '🚽';
+
+      expect(
+        const SourceException(source: source, reason: reason).message,
+        'Source "$source" is invalid -- $reason',
+      );
+    });
+  });
+
+  group('$BrickException', () {
+    test('can be instanciated', () {
+      expect(
+        const BrickException(brick: 'test', reason: 'test'),
+        isA<BrickOvenException>(),
+      );
+
+      expect(
+        () => const BrickException(brick: 'test', reason: 'test'),
+        returnsNormally,
+      );
+    });
+
+    test('has the correct message', () {
+      const brick = '💩';
+      const reason = '🚽';
+
+      expect(
+        const BrickException(brick: brick, reason: reason).message,
+        'Brick "$brick" is invalid -- $reason',
+      );
+    });
+  });
+
+  group('$FileException', () {
+    test('can be instanciated', () {
+      expect(
+        const FileException(file: 'test', reason: 'test'),
+        isA<BrickOvenException>(),
+      );
+
+      expect(
+        () => const FileException(file: 'test', reason: 'test'),
+        returnsNormally,
+      );
+    });
+
+    test('has the correct message', () {
+      const file = '💩';
+      const reason = '🚽';
+
+      expect(
+        const FileException(file: file, reason: reason).message,
+        'File "$file" is invalid -- $reason',
+      );
+    });
+  });
+
   group('$MaxUpdateException', () {
     test('can be instanciated', () {
       expect(const MaxUpdateException(1), isA<BrickOvenException>());
