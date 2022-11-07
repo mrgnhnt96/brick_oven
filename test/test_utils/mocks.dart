27130ff2fc6735f77ef38c9af0cd9449d6df4805
Morgan <mrgnhnt96@gmail.com>
2022-11-07 17:14:18 -0600

    refactor: add mock file watcher

--- test/test_utils/mocks.dart
+++ test/test_utils/mocks.dart
@@ -5,6 +5,7 @@ import 'package:pub_updater/pub_updater.dart';
 
 import 'package:brick_oven/domain/brick.dart';
 import 'package:brick_oven/domain/brick_watcher.dart';
+import 'package:watcher/watcher.dart';
 
 class MockBrick extends Mock implements Brick {}
 
@@ -17,3 +18,5 @@ class MockBrickWatcher extends Mock implements BrickWatcher {}
 class MockProgress extends Mock implements Progress {}
 
 class MockBrickYamlConfig extends Mock implements BrickYamlConfig {}
+
+class MockFileWatcher extends Mock implements FileWatcher {}
