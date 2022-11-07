670ea7db7fafa91bcd2b5e874707119a18d30b63
Morgan <mrgnhnt96@gmail.com>
2022-11-07 16:25:12 -0600

    refactor: remove unneeded exceptions

--- lib/src/exception.dart
+++ lib/src/exception.dart
@@ -1,5 +1,3 @@
-import 'package:brick_oven/domain/brick_oven_yaml.dart';
-
 /// {@template brick_oven_exception}
 /// An exception thrown by an internal brick oven command.
 /// {@endtemplate}
@@ -9,50 +7,6 @@ class BrickOvenException implements Exception {
 
   /// The error message which will be displayed to the user via stderr.
   final String message;
-
-  @override
-  String toString() => message;
-}
-
-/// {@template brick_oven_not_found_exception}
-/// An exception thrown when a brick oven configuration file is not found.
-/// {@endtemplate}
-class BrickOvenNotFoundException extends BrickOvenException {
-  /// {@macro brick_oven_not_found_exception}
-  const BrickOvenNotFoundException()
-      // coverage:ignore-start
-      : super(
-          // coverage:ignore-end
-          'Cannot find ${BrickOvenYaml.file}.'
-          '\nCreate the file and try again.',
-        );
-}
-
-/// {@template brick_oven_not_found_exception}
-/// An exception thrown when a brick is not found.
-/// {@endtemplate}
-class BrickNotFoundException extends BrickOvenException {
-  /// {@macro brick_oven_not_found_exception}
-  const BrickNotFoundException(String brick)
-      // coverage:ignore-start
-      : super(
-          // coverage:ignore-end
-          'Cannot find $brick.\n'
-          'Make sure to provide a valid brick name '
-          'from the ${BrickOvenYaml.file}.',
-        );
-}
-
-/// {@template unknown_keys_exception}
-/// An exception thrown when an unknown key is found in a brick oven
-/// configuration file.
-/// {@endtemplate}
-class UnknownKeysException extends BrickOvenException {
-  /// {@macro unknown_keys_exception}
-  UnknownKeysException(
-    Iterable<String> keys,
-    String location,
-  ) : super('Unknown keys: ${keys.join(', ')}, in $location');
 }
 
 /// {@template config_exception}
--- test/src/exception_test.dart
+++ test/src/exception_test.dart
@@ -1,62 +1,10 @@
 import 'package:test/test.dart';
 
-import 'package:brick_oven/domain/brick_oven_yaml.dart';
 import 'package:brick_oven/src/exception.dart';
 
 void main() {
   test('$BrickOvenException can be instanciated', () {
     expect(() => const BrickOvenException('test'), returnsNormally);
-  });
-
-  group('$BrickOvenNotFoundException', () {
-    test('can be instanciated', () {
-      expect(const BrickOvenNotFoundException(), isA<BrickOvenException>());
-
-      expect(() => const BrickOvenNotFoundException(), returnsNormally);
-    });
-
-    test('has the correct message', () {
-      expect(
-        const BrickOvenNotFoundException().message,
-        'Cannot find ${BrickOvenYaml.file}.'
-        '\nCreate the file and try again.',
-      );
-    });
-  });
-
-  group('$BrickNotFoundException', () {
-    test('can be instanciated', () {
-      expect(const BrickNotFoundException('brick'), isA<BrickOvenException>());
-
-      expect(() => const BrickNotFoundException('brick'), returnsNormally);
-    });
-
-    test('has the correct message', () {
-      const brick = 'test';
-      expect(
-        const BrickNotFoundException(brick).message,
-        'Cannot find $brick.\n'
-        'Make sure to provide a valid brick name '
-        'from the ${BrickOvenYaml.file}.',
-      );
-    });
-  });
-
-  group('$UnknownKeysException', () {
-    test('can be instanciated', () {
-      expect(UnknownKeysException(['test'], 'loc'), isA<BrickOvenException>());
-
-      expect(() => UnknownKeysException(['test'], 'loc'), returnsNormally);
-    });
-
-    test('has the correct message', () {
-      const keys = ['test', 'test2'];
-      const location = 'location';
-      expect(
-        UnknownKeysException(keys, location).message,
-        'Unknown keys: ${keys.join(', ')}, in $location',
-      );
-    });
   });
 
   group('$VariableException', () {
