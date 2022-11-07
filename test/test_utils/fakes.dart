32370c27b4c6ff5453e12a7a4a80868ae8f27991
Morgan <mrgnhnt96@gmail.com>
2022-11-07 17:13:59 -0600

    refactor: remove unneeded code

--- test/test_utils/fakes.dart
+++ test/test_utils/fakes.dart
@@ -1,7 +1,4 @@
 import 'package:args/args.dart';
-import 'package:brick_oven/domain/brick.dart';
-import 'package:brick_oven/domain/brick_source.dart';
-import 'package:brick_oven/domain/brick_watcher.dart';
 import 'package:mocktail/mocktail.dart';
 
 class FakeArgResults extends Fake implements ArgResults {
@@ -13,14 +10,5 @@ class FakeArgResults extends Fake implements ArgResults {
   dynamic operator [](String key) => data[key];
 }
 
-class FakeBrick extends Fake implements Brick {}
-
 // ignore: prefer_function_declarations_over_variables
 final void Function() voidCallback = () {};
-
-class FakeBrickSource extends Fake implements BrickSource {
-  FakeBrickSource(this.watcher);
-
-  @override
-  final BrickWatcher watcher;
-}
