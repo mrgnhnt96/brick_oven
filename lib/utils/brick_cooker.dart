ed1d26e2a9c8c9a2a760b35d12e000c867bbe7cd
Morgan <mrgnhnt96@gmail.com>
2022-11-12 12:04:59 -0600

    feat: create brick cooker args

--- lib/utils/brick_cooker.dart
+++ lib/utils/brick_cooker.dart
@@ -1,24 +1,30 @@
+import 'package:args/command_runner.dart';
 import 'package:brick_oven/domain/brick.dart';
 import 'package:brick_oven/domain/brick_oven_yaml.dart';
 import 'package:brick_oven/src/key_press_listener.dart';
-import 'package:brick_oven/utils/config_watcher_mixin.dart';
 import 'package:mason_logger/mason_logger.dart';
 
 /// {@template brick_cooker}
 /// A base class for `BrickOvenCommand`s that cook bricks.
 /// {@endtemplate}
-abstract class BrickCooker with ConfigWatcherMixin {
+mixin BrickCooker {
   /// the logger for output messages
   Logger get logger;
 
+  /// {@macro key_press_listener}
+  KeyPressListener? get keyPressListener;
+}
+
+/// {@template brick_cooker_args}
+/// The args for a [BrickCooker].
+/// {@endtemplate}
+mixin BrickCookerArgs on Command<int> {
   /// whether to listen to changes to the
   /// [BrickOvenYaml.file] or the [Brick.configPath] file and
   /// the [Brick.source] directory
-  bool get isWatch;
+
+  bool get isWatch => argResults?['watch'] == true;
 
   /// the directory to output the brick to
-  String? get outputDir;
-
-  /// {@macro key_press_listener}
-  KeyPressListener? get keyPressListener;
+  String? get outputDir => argResults?['output'] as String?;
 }
