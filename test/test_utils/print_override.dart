a12a49c8fbb6265f3d85cc7a7717517006858fa4
Morgan <mrgnhnt96@gmail.com>
2022-11-17 14:36:57 -0600

    refactor: invoke method immediately

--- test/test_utils/print_override.dart
+++ test/test_utils/print_override.dart
@@ -2,14 +2,14 @@ import 'dart:async';
 
 List<String> printLogs = <String>[];
 
-void Function() overridePrint(void Function() fn) {
-  return () {
-    final spec = ZoneSpecification(
-      print: (_, __, ___, String msg) {
-        printLogs.add(msg);
-      },
-    );
+Future<void> overridePrint(FutureOr<void> Function() fn) async {
+  final spec = ZoneSpecification(
+    print: (_, __, ___, String msg) {
+      printLogs.add(msg);
+    },
+  );
 
-    return Zone.current.fork(specification: spec).run<void>(fn);
-  };
+  final zone = Zone.current.fork(specification: spec);
+
+  await zone.run(fn);
 }
