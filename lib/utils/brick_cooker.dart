1e28c556a483e850bebb4ff1a938c5b5f81d396f
Morgan <mrgnhnt96@gmail.com>
2022-11-17 20:18:04 -0600

    fix: update check for boolean and add default values

--- lib/utils/brick_cooker.dart
+++ lib/utils/brick_cooker.dart
@@ -22,10 +22,10 @@ mixin BrickCookerArgs on Command<int> {
   /// whether to listen to changes to the
   /// [BrickOvenYaml.file] or the [Brick.configPath] file and
   /// the [Brick.source] directory
-  bool get isWatch => argResults?['watch'] == true;
+  bool get isWatch => argResults?['watch'] as bool? ?? false;
 
   /// whether to validate if the brick.yaml file is synced
-  bool get shouldSync => argResults?['sync'] == true;
+  bool get shouldSync => argResults?['sync'] as bool? ?? true;
 
   /// the directory to output the brick to
   String? get outputDir => argResults?['output'] as String?;
