147e89f88771bfac8b0571b68f06e75ca2397fe4
Morgan <mrgnhnt96@gmail.com>
2022-11-18 01:55:26 -0600

    feat: add if, ifNot, and endIf, add Case to mustache, add ifValue getters, isFormat getter, and tests for all refactor: update logic for suffixFrom, findFrom

--- lib/enums/mustache_format.dart
+++ lib/enums/mustache_format.dart
@@ -18,7 +18,7 @@ enum MustacheFormat {
   lowerCase,
 
   /// {{ mustache }}
-  mustache,
+  mustacheCase,
 
   /// Pascal Case
   pascalCase,
@@ -43,6 +43,15 @@ enum MustacheFormat {
 
   /// no format, but wrapped with `{{{}}}`
   escape,
+
+  /// start of an inverted inline section
+  ifNot,
+
+  /// start of an inline section
+  if_,
+
+  /// end of an inline section
+  endIf,
 }
 
 /// the extensions for Mustache
@@ -63,62 +72,131 @@ extension MustacheFormatX on MustacheFormat {
     return '{{#$name}}$content{{/$name}}';
   }
 
+  /// whether the value is a format type
+  bool get isFormat {
+    if (isCamelCase) return true;
+    if (isConstantCase) return true;
+    if (isDotCase) return true;
+    if (isHeaderCase) return true;
+    if (isLowerCase) return true;
+    if (isMustacheCase) return true;
+    if (isPascalCase) return true;
+    if (isParamCase) return true;
+    if (isPathCase) return true;
+    if (isSentenceCase) return true;
+    if (isSnakeCase) return true;
+    if (isTitleCase) return true;
+    if (isUpperCase) return true;
+
+    return false;
+  }
+
   /// whether the format is [MustacheFormat.escape]
   bool get isEscape => this == MustacheFormat.escape;
-}
 
-/// The extensions for Lists
-extension ListX<T> on List<T> {
-  /// loops through looking for a matched [value], return null if not found
-  T? retrieve(Object? value) {
-    for (final e in this) {
-      if (e is Enum) {
-        if (e.name.toLowerCase() == '$value'.toLowerCase()) {
-          return e;
-        }
-      } else if (e is String) {
-        if (e.toLowerCase() == '$value'.toLowerCase()) {
-          return e;
-        }
-      } else {
-        if (e == value) {
-          return e;
-        }
-      }
-    }
+  /// whether the format is [MustacheFormat.if_]
+  bool get isIf => this == MustacheFormat.if_;
 
-    return null;
-  }
+  /// whether the format is [MustacheFormat.ifNot]
+  bool get isIfNot => this == MustacheFormat.ifNot;
+
+  /// whether the format is [MustacheFormat.endIf]
+  bool get isEndIf => this == MustacheFormat.endIf;
+
+  /// whether the value is [MustacheFormat.camelCase]
+  bool get isCamelCase => this == MustacheFormat.camelCase;
+
+  /// whether the value is [MustacheFormat.constantCase]
+  bool get isConstantCase => this == MustacheFormat.constantCase;
+
+  /// whether the value is [MustacheFormat.dotCase]
+  bool get isDotCase => this == MustacheFormat.dotCase;
+
+  /// whether the value is [MustacheFormat.headerCase]
+  bool get isHeaderCase => this == MustacheFormat.headerCase;
+
+  /// whether the value is [MustacheFormat.lowerCase]
+  bool get isLowerCase => this == MustacheFormat.lowerCase;
+
+  /// whether the value is [MustacheFormat.mustacheCase]
+  bool get isMustacheCase => this == MustacheFormat.mustacheCase;
+
+  /// whether the value is [MustacheFormat.pascalCase]
+  bool get isPascalCase => this == MustacheFormat.pascalCase;
+
+  /// whether the value is [MustacheFormat.paramCase]
+  bool get isParamCase => this == MustacheFormat.paramCase;
+
+  /// whether the value is [MustacheFormat.pathCase]
+  bool get isPathCase => this == MustacheFormat.pathCase;
+
+  /// whether the value is [MustacheFormat.sentenceCase]
+  bool get isSentenceCase => this == MustacheFormat.sentenceCase;
+
+  /// whether the value is [MustacheFormat.snakeCase]
+  bool get isSnakeCase => this == MustacheFormat.snakeCase;
+
+  /// whether the value is [MustacheFormat.titleCase]
+  bool get isTitleCase => this == MustacheFormat.titleCase;
+
+  /// whether the value is [MustacheFormat.upperCase]
+  bool get isUpperCase => this == MustacheFormat.upperCase;
 }
 
 /// extension on [List<MustacheFormat>]
 extension ListMustacheX on List<MustacheFormat> {
   /// loops through looking for a matched [value], return null if not found
-  MustacheFormat? getMustacheValue(String? value) {
+  MustacheFormat? findFrom(String? value) {
+    if (value == null) {
+      return null;
+    }
+
+    final valueLower = value.toLowerCase();
+
     for (final e in this) {
-      if ('$value'.toLowerCase().startsWith(e.name.toLowerCase())) {
+      final name = e.name.toLowerCase().replaceAll('_', '');
+
+      if (valueLower.startsWith(name)) {
         return e;
-      } else if ('$value'
-          .toLowerCase()
-          .startsWith(e.name.replaceAll('Case', '').toLowerCase())) {
+      }
+
+      if (!e.isFormat) {
+        continue;
+      }
+
+      if (valueLower.startsWith(e.name.replaceAll('Case', ''))) {
         return e;
       }
     }
+
     return null;
   }
 
   /// returns the suffix of the [value] by removing the [MustacheFormat]
-  String? getSuffix(String? value) {
-    String? withoutCase;
-    for (final e in this) {
-      if ('$value'.toLowerCase().startsWith(e.name.toLowerCase())) {
-        return value?.substring(e.name.length);
-      } else if ('$value'.toLowerCase().startsWith(
-            (withoutCase = e.name.replaceAll('Case', '')).toLowerCase(),
-          )) {
-        return value?.substring(withoutCase.length);
-      }
+  String? suffixFrom(String? value) {
+    if (value == null) {
+      return null;
     }
+
+    final mustacheFormat = findFrom(value);
+
+    if (mustacheFormat == null) {
+      return null;
+    }
+
+    final mustacheName = mustacheFormat.name.toLowerCase().replaceAll('_', '');
+    final valueLower = value.toLowerCase();
+
+    if (valueLower.startsWith(mustacheName)) {
+      return value.substring(mustacheName.length);
+    }
+
+    final shortMustacheName = mustacheName.replaceAll('case', '');
+
+    if (valueLower.startsWith(shortMustacheName)) {
+      return value.substring(shortMustacheName.length);
+    }
+
     return null;
   }
 }
--- test/enums/mustache_format_test.dart
+++ test/enums/mustache_format_test.dart
@@ -3,6 +3,197 @@ import 'package:test/test.dart';
 import 'package:brick_oven/enums/mustache_format.dart';
 
 void main() {
+  const formats = [
+    MustacheFormat.camelCase,
+    MustacheFormat.constantCase,
+    MustacheFormat.dotCase,
+    MustacheFormat.headerCase,
+    MustacheFormat.lowerCase,
+    MustacheFormat.mustacheCase,
+    MustacheFormat.pascalCase,
+    MustacheFormat.paramCase,
+    MustacheFormat.pathCase,
+    MustacheFormat.sentenceCase,
+    MustacheFormat.snakeCase,
+    MustacheFormat.titleCase,
+    MustacheFormat.upperCase,
+  ];
+
+  const nonFormats = [
+    MustacheFormat.escape,
+    MustacheFormat.if_,
+    MustacheFormat.ifNot,
+    MustacheFormat.endIf,
+  ];
+
+  const suffix = 'ASDF';
+
+  const formatsWithoutSuffixes = {
+    MustacheFormat.camelCase: [
+      'camelCase',
+      'camelcase',
+      'camel',
+    ],
+    MustacheFormat.constantCase: [
+      'constantCase',
+      'constantcase',
+      'constant',
+    ],
+    MustacheFormat.dotCase: [
+      'dotCase',
+      'dotcase',
+      'dot',
+    ],
+    MustacheFormat.headerCase: [
+      'headerCase',
+      'headercase',
+      'header',
+    ],
+    MustacheFormat.lowerCase: [
+      'lowerCase',
+      'lowercase',
+      'lower',
+    ],
+    MustacheFormat.mustacheCase: [
+      'mustacheCase',
+      'mustachecase',
+      'mustache',
+    ],
+    MustacheFormat.pascalCase: [
+      'pascalCase',
+      'pascalcase',
+      'pascal',
+    ],
+    MustacheFormat.paramCase: [
+      'paramCase',
+      'paramcase',
+      'param',
+    ],
+    MustacheFormat.pathCase: [
+      'pathCase',
+      'pathcase',
+      'path',
+    ],
+    MustacheFormat.sentenceCase: [
+      'sentenceCase',
+      'sentencecase',
+      'sentence',
+    ],
+    MustacheFormat.snakeCase: [
+      'snakeCase',
+      'snakecase',
+      'snake',
+    ],
+    MustacheFormat.titleCase: [
+      'titleCase',
+      'titlecase',
+      'title',
+    ],
+    MustacheFormat.upperCase: [
+      'upperCase',
+      'uppercase',
+      'upper',
+    ],
+    MustacheFormat.escape: [
+      'escape',
+    ],
+    MustacheFormat.if_: [
+      'if',
+    ],
+    MustacheFormat.ifNot: [
+      'ifNot',
+      'ifnot',
+    ],
+    MustacheFormat.endIf: [
+      'endIf',
+      'endif',
+    ],
+  };
+
+  const formatsWithSuffixes = {
+    MustacheFormat.camelCase: [
+      'camelCase$suffix',
+      'camelcase$suffix',
+      'camel$suffix',
+    ],
+    MustacheFormat.constantCase: [
+      'constantCase$suffix',
+      'constantCase$suffix',
+      'constant$suffix',
+    ],
+    MustacheFormat.dotCase: [
+      'dotCase$suffix',
+      'dotcase$suffix',
+      'dot$suffix',
+    ],
+    MustacheFormat.headerCase: [
+      'headerCase$suffix',
+      'headercase$suffix',
+      'header$suffix',
+    ],
+    MustacheFormat.lowerCase: [
+      'lowerCase$suffix',
+      'lowercase$suffix',
+      'lower$suffix',
+    ],
+    MustacheFormat.mustacheCase: [
+      'mustacheCase$suffix',
+      'mustacheCase$suffix',
+      'mustache$suffix',
+    ],
+    MustacheFormat.pascalCase: [
+      'pascalCase$suffix',
+      'pascalcase$suffix',
+      'pascal$suffix',
+    ],
+    MustacheFormat.paramCase: [
+      'paramCase$suffix',
+      'paramcase$suffix',
+      'param$suffix',
+    ],
+    MustacheFormat.pathCase: [
+      'pathCase$suffix',
+      'pathcase$suffix',
+      'path$suffix',
+    ],
+    MustacheFormat.sentenceCase: [
+      'sentenceCase$suffix',
+      'sentenceCase$suffix',
+      'sentence$suffix',
+    ],
+    MustacheFormat.snakeCase: [
+      'snakeCase$suffix',
+      'snakecase$suffix',
+      'snake$suffix',
+    ],
+    MustacheFormat.titleCase: [
+      'titleCase$suffix',
+      'titlecase$suffix',
+      'title$suffix',
+    ],
+    MustacheFormat.upperCase: [
+      'upperCase$suffix',
+      'uppercase$suffix',
+      'upper$suffix',
+    ],
+    MustacheFormat.escape: [
+      'escape$suffix',
+    ],
+    MustacheFormat.if_: [
+      'if$suffix',
+    ],
+    MustacheFormat.ifNot: [
+      'ifNot$suffix',
+      'ifnot$suffix',
+    ],
+    MustacheFormat.endIf: [
+      'endIf$suffix',
+      'endif$suffix',
+    ],
+  };
+
+  const allFormats = [...formats, ...nonFormats];
+
   group('MustacheFormatX', () {
     group('#toMustache', () {
       test('throws assertion when content is not wrapped with brackets', () {
@@ -31,34 +222,116 @@ void main() {
         }
       });
     });
+
+    test('#isFormat returns true when the value is a format type', () {
+      for (final format in formats) {
+        expect(format.isFormat, isTrue);
+      }
+
+      for (final format in nonFormats) {
+        expect(format.isFormat, isFalse);
+      }
+
+      const allValues = MustacheFormat.values;
+
+      for (final format in allValues) {
+        expect(allFormats, contains(format));
+      }
+    });
+
+    test('#isValue is correct for all values', () {
+      final values = {
+        MustacheFormat.camelCase: () => MustacheFormat.camelCase.isCamelCase,
+        MustacheFormat.constantCase: () =>
+            MustacheFormat.constantCase.isConstantCase,
+        MustacheFormat.dotCase: () => MustacheFormat.dotCase.isDotCase,
+        MustacheFormat.headerCase: () => MustacheFormat.headerCase.isHeaderCase,
+        MustacheFormat.lowerCase: () => MustacheFormat.lowerCase.isLowerCase,
+        MustacheFormat.mustacheCase: () =>
+            MustacheFormat.mustacheCase.isMustacheCase,
+        MustacheFormat.pascalCase: () => MustacheFormat.pascalCase.isPascalCase,
+        MustacheFormat.paramCase: () => MustacheFormat.paramCase.isParamCase,
+        MustacheFormat.pathCase: () => MustacheFormat.pathCase.isPathCase,
+        MustacheFormat.sentenceCase: () =>
+            MustacheFormat.sentenceCase.isSentenceCase,
+        MustacheFormat.snakeCase: () => MustacheFormat.snakeCase.isSnakeCase,
+        MustacheFormat.titleCase: () => MustacheFormat.titleCase.isTitleCase,
+        MustacheFormat.upperCase: () => MustacheFormat.upperCase.isUpperCase,
+        MustacheFormat.escape: () => MustacheFormat.escape.isEscape,
+        MustacheFormat.if_: () => MustacheFormat.if_.isIf,
+        MustacheFormat.ifNot: () => MustacheFormat.ifNot.isIfNot,
+        MustacheFormat.endIf: () => MustacheFormat.endIf.isEndIf,
+      };
+
+      final keys = values.keys;
+
+      for (final key in MustacheFormat.values) {
+        expect(keys, contains(key));
+      }
+
+      for (final key in keys) {
+        expect(values[key]!(), isTrue);
+      }
+    });
   });
 
-  group('ListX', () {
-    group('#retrieve', () {
-      test('enum returns the value when provided', () {
-        final result = MustacheFormat.values.retrieve('camelCase');
+  group('ListMustacheX', () {
+    group('#findFrom', () {
+      test('returns when the correct enum for value', () {
+        final keys = formatsWithoutSuffixes.keys;
 
-        expect(result, MustacheFormat.camelCase);
+        for (final key in MustacheFormat.values) {
+          expect(keys, contains(key));
+        }
+
+        for (final key in keys) {
+          for (final value in formatsWithoutSuffixes[key]!) {
+            final result = MustacheFormat.values.findFrom(value);
+            expect(result, key);
+          }
+        }
       });
 
-      test('string returns the value when provided', () {
-        const search = 'num';
-        final result = ['val', search].retrieve(search);
+      test('returns appropriate value when provided with suffixes', () {
+        final keys = formatsWithSuffixes.keys;
 
-        expect(result, search);
+        for (final key in MustacheFormat.values) {
+          expect(keys, contains(key));
+        }
+
+        for (final key in keys) {
+          for (final value in formatsWithSuffixes[key]!) {
+            final result = MustacheFormat.values.findFrom(value);
+            expect(result, key);
+          }
+        }
       });
 
-      test('int returns the value when provided', () {
-        const search = 1;
-        final result = [2, search].retrieve(search);
+      test('returns null when value is not found', () {
+        expect(MustacheFormat.values.findFrom('nothing'), isNull);
+        expect(MustacheFormat.values.findFrom(null), isNull);
+      });
+    });
 
-        expect(result, search);
+    group('#suffixFrom', () {
+      test('returns the suffix for the provided value', () {
+        final keys = formatsWithSuffixes.keys;
+
+        for (final key in MustacheFormat.values) {
+          expect(keys, contains(key));
+        }
+
+        for (final key in keys) {
+          for (final value in formatsWithSuffixes[key]!) {
+            final result = MustacheFormat.values.suffixFrom(value);
+            expect(result, suffix);
+          }
+        }
       });
 
-      test('returns null when not provided', () {
-        final result = MustacheFormat.values.retrieve('not_a_format');
-
-        expect(result, isNull);
+      test('returns null when value is not found', () {
+        expect(MustacheFormat.values.suffixFrom('nothing'), isNull);
+        expect(MustacheFormat.values.suffixFrom(null), isNull);
       });
     });
   });
