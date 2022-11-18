8fe0d9fefe72bd01329be89d3eec510568d7a8f3
Morgan <mrgnhnt96@gmail.com>
2022-11-17 23:47:45 -0600

    feat: add tests for run gracefully

--- test/src/commands/cook_bricks/cook_all_bricks_test.dart
+++ test/src/commands/cook_bricks/cook_all_bricks_test.dart
@@ -1,15 +1,20 @@
+// ignore_for_file: avoid_redundant_argument_values
+
 import 'dart:async';
 
 import 'package:args/args.dart';
+import 'package:brick_oven/domain/brick.dart';
 import 'package:brick_oven/domain/brick_or_error.dart';
 import 'package:brick_oven/src/commands/cook_bricks/cook_all_bricks.dart';
+import 'package:brick_oven/src/exception.dart';
+import 'package:brick_oven/src/key_press_listener.dart';
+import 'package:brick_oven/utils/extensions/logger_extensions.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:usage/usage_io.dart';
-import 'package:watcher/watcher.dart';
 
 import '../../../test_utils/fakes.dart';
 import '../../../test_utils/mocks.dart';
@@ -20,8 +25,10 @@ void main() {
   late Progress mockProgress;
   late FileSystem memoryFileSystem;
   late Analytics mockAnalytics;
+  late Brick mockBrick;
 
   setUp(() {
+    mockBrick = MockBrick();
     mockLogger = MockLogger();
     mockProgress = MockProgress();
     mockAnalytics = MockAnalytics()..stubMethods();
@@ -47,21 +54,290 @@ void main() {
     });
 
     group('#run', () {
-      test('gracefully', () async {
-        final command = TestCookAllBricks(
-          analytics: mockAnalytics,
-          bricksOrError: const BrickOrError({}, null),
-          logger: mockLogger,
-          fileSystem: memoryFileSystem,
-        );
+      group('gracefully', () {
+        test('when shouldSync and isWatch are default values', () async {
+          final command = TestCookAllBricks(
+            analytics: mockAnalytics,
+            bricksOrError: BrickOrError({mockBrick}, null),
+            logger: mockLogger,
+            fileSystem: memoryFileSystem,
+          );
 
-        final result = await command.run();
+          final result = await command.run();
 
-        expect(result, ExitCode.success.code);
+          verify(mockBrick.cook).called(1);
+
+          verify(mockLogger.preheat).called(1);
+          verify(mockLogger.dingDing).called(1);
+
+          verify(
+            () => mockAnalytics.sendEvent(
+              'cook',
+              'all',
+              label: 'no-watch',
+              value: 0,
+              parameters: {
+                'bricks': '1',
+                'sync': 'true',
+              },
+            ),
+          ).called(1);
+
+          verify(
+            () => mockAnalytics.waitForLastPing(
+              timeout: any(named: 'timeout'),
+            ),
+          ).called(1);
+
+          expect(result, ExitCode.success.code);
+        });
+
+        group('when #shouldSync', () {
+          test('is false', () async {
+            final command = TestCookAllBricks(
+              analytics: mockAnalytics,
+              bricksOrError: BrickOrError({mockBrick}, null),
+              logger: mockLogger,
+              shouldSync: false,
+              fileSystem: memoryFileSystem,
+            );
+
+            final result = await command.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: false,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'all',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'false',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+            verifyNoMoreInteractions(mockAnalytics);
+
+            expect(result, ExitCode.success.code);
+          });
+
+          test('is true', () async {
+            final command = TestCookAllBricks(
+              analytics: mockAnalytics,
+              bricksOrError: BrickOrError({mockBrick}, null),
+              logger: mockLogger,
+              shouldSync: true,
+              fileSystem: memoryFileSystem,
+            );
+
+            final result = await command.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'all',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+            verifyNoMoreInteractions(mockAnalytics);
+
+            expect(result, ExitCode.success.code);
+          });
+        });
+
+        group('when #isWatch', () {
+          late Completer<int> exitCompleter;
+          late KeyPressListener keyPressListener;
+
+          setUp(() {
+            exitCompleter = Completer<int>();
+
+            final mockStdin = MockStdin();
+
+            when(() => mockStdin.hasTerminal).thenReturn(true);
+
+            when(mockStdin.asBroadcastStream).thenAnswer(
+              (_) => Stream.fromIterable([
+                'q'.codeUnits,
+              ]),
+            );
+
+            keyPressListener = KeyPressListener(
+              stdin: mockStdin,
+              logger: mockLogger,
+              toExit: exitCompleter.complete,
+            );
+
+            final mockSource = MockSource();
+            final mockWatcher = MockWatcher();
+
+            when(() => mockBrick.source).thenReturn(mockSource);
+            when(() => mockSource.watcher).thenReturn(mockWatcher);
+
+            when(() => mockWatcher.hasRun).thenAnswer((_) => true);
+            when(mockWatcher.start).thenAnswer((_) => Future.value());
+            when(mockWatcher.stop).thenAnswer((_) => Future.value());
+          });
+
+          tearDown(() {
+            KeyPressListener.stream = null;
+          });
+
+          test('is false', () async {
+            final command = TestCookAllBricks(
+              analytics: mockAnalytics,
+              bricksOrError: BrickOrError({mockBrick}, null),
+              logger: mockLogger,
+              isWatch: false,
+              fileSystem: memoryFileSystem,
+              keyPressListener: keyPressListener,
+            );
+
+            final result = await command.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'all',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+            verifyNoMoreInteractions(mockAnalytics);
+
+            expect(result, ExitCode.success.code);
+          });
+
+          test('is true', () async {
+            final command = TestCookAllBricks(
+              analytics: mockAnalytics,
+              bricksOrError: BrickOrError({mockBrick}, null),
+              logger: mockLogger,
+              isWatch: true,
+              fileSystem: memoryFileSystem,
+              keyPressListener: keyPressListener,
+            );
+
+            final runResult = await command.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: true,
+              ),
+            ).called(1);
+            verify(() => mockBrick.configPath).called(1);
+            verify(() => mockBrick.source).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+            verify(mockLogger.watching).called(1);
+            verify(mockLogger.quit).called(1);
+            verify(mockLogger.reload).called(1);
+            verify(mockLogger.exiting).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'all',
+                label: 'watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+            verifyNoMoreInteractions(mockAnalytics);
+
+            final result = await exitCompleter.future;
+
+            expect(result, ExitCode.success.code);
+            expect(runResult, ExitCode.success.code);
+          });
+        });
       });
 
-      test('returns ${ExitCode.config.code} when bricks returns an error',
-          () async {
+      test('when error occurs when parsing bricks', () async {
         final command = TestCookAllBricks(
           analytics: mockAnalytics,
           bricksOrError: const BrickOrError(null, 'error'),
@@ -71,7 +347,101 @@ void main() {
 
         final result = await command.run();
 
+        verify(() => mockLogger.err('error')).called(1);
+
         expect(result, ExitCode.config.code);
+      });
+
+      test('when unknown error occurs', () async {
+        when(() => mockBrick.name).thenReturn('BRICK');
+        when(() => mockBrick.cook()).thenThrow(Exception('error'));
+
+        final command = TestCookAllBricks(
+          analytics: mockAnalytics,
+          bricksOrError: BrickOrError({mockBrick}, null),
+          logger: mockLogger,
+          fileSystem: memoryFileSystem,
+        );
+
+        final result = await command.run();
+
+        verify(mockBrick.cook).called(1);
+
+        verify(mockLogger.preheat).called(1);
+        verify(() => mockLogger.warn('Unknown error: Exception: error'))
+            .called(1);
+        verify(() => mockLogger.err('Could not cook brick: BRICK')).called(1);
+        verify(mockLogger.dingDing).called(1);
+
+        verify(
+          () => mockAnalytics.sendEvent(
+            'cook',
+            'all',
+            label: 'no-watch',
+            value: 0,
+            parameters: {
+              'bricks': '1',
+              'sync': 'true',
+            },
+          ),
+        ).called(1);
+
+        verify(
+          () => mockAnalytics.waitForLastPing(
+            timeout: any(named: 'timeout'),
+          ),
+        ).called(1);
+
+        expect(result, ExitCode.success.code);
+      });
+
+      test('when config error occurs', () async {
+        when(() => mockBrick.name).thenReturn('BRICK');
+        when(() => mockBrick.cook())
+            .thenThrow(const BrickException(brick: 'BRICK', reason: 'error'));
+
+        final command = TestCookAllBricks(
+          analytics: mockAnalytics,
+          bricksOrError: BrickOrError({mockBrick}, null),
+          logger: mockLogger,
+          fileSystem: memoryFileSystem,
+        );
+
+        final result = await command.run();
+
+        verify(mockBrick.cook).called(1);
+
+        verify(mockLogger.preheat).called(1);
+
+        verify(
+          () => mockLogger.warn(
+            'Invalid brick config: "BRICK"\n'
+            'Reason: error',
+          ),
+        ).called(1);
+        verify(() => mockLogger.err('Could not cook brick: BRICK')).called(1);
+        verify(mockLogger.dingDing).called(1);
+
+        verify(
+          () => mockAnalytics.sendEvent(
+            'cook',
+            'all',
+            label: 'no-watch',
+            value: 0,
+            parameters: {
+              'bricks': '1',
+              'sync': 'true',
+            },
+          ),
+        ).called(1);
+
+        verify(
+          () => mockAnalytics.waitForLastPing(
+            timeout: any(named: 'timeout'),
+          ),
+        ).called(1);
+
+        expect(result, ExitCode.success.code);
       });
     });
   });
@@ -82,34 +452,42 @@ class TestCookAllBricks extends CookAllBricks {
     required Logger logger,
     required FileSystem fileSystem,
     this.bricksOrError,
-    this.fileWatchers,
+    bool? isWatch,
+    bool? shouldSync,
     Map<String, dynamic>? argResults,
     required Analytics analytics,
+    KeyPressListener? keyPressListener,
   })  : _argResults = argResults ?? <String, dynamic>{},
+        _isWatch = isWatch,
+        _shouldSync = shouldSync,
         super(
           logger: logger,
           fileSystem: fileSystem,
           analytics: analytics,
+          keyPressListener: keyPressListener,
         );
 
   final Map<String, dynamic> _argResults;
-  final List<FileWatcher?>? fileWatchers;
+
   final BrickOrError? bricksOrError;
+  final bool? _isWatch;
+  final bool? _shouldSync;
+
+  @override
+  bool get isWatch => _isWatch ?? super.isWatch;
+
+  @override
+  bool get shouldSync => _shouldSync ?? super.shouldSync;
 
   @override
   BrickOrError bricks() => bricksOrError ?? const BrickOrError({}, null);
 
-  int _callCount = 0;
-
   @override
-  FileWatcher watcher(String path) {
-    if (fileWatchers != null && fileWatchers!.length > _callCount) {
-      final mock = MockFileWatcher();
-      when(() => mock.events).thenAnswer((_) => const Stream.empty());
-
-      return fileWatchers![_callCount++] ?? mock;
-    }
-    return super.watcher(path);
+  Future<bool> watchForConfigChanges(
+    String _, {
+    FutureOr<void> Function()? onChange,
+  }) async {
+    return true;
   }
 
   @override
--- test/src/commands/cook_bricks/cook_single_brick_test.dart
+++ test/src/commands/cook_bricks/cook_single_brick_test.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_redundant_argument_values
+
+import 'dart:async';
 import 'dart:io';
 
 import 'package:brick_oven/domain/brick.dart';
@@ -5,6 +8,7 @@ import 'package:brick_oven/domain/brick_source.dart';
 import 'package:brick_oven/domain/brick_watcher.dart';
 import 'package:brick_oven/src/commands/cook_bricks/cook_single_brick.dart';
 import 'package:brick_oven/src/exception.dart';
+import 'package:brick_oven/src/key_press_listener.dart';
 import 'package:brick_oven/utils/extensions/logger_extensions.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
@@ -92,43 +96,294 @@ void main() {
     });
 
     group('#run', () {
-      test('gracefully', () async {
-        final runner = CookSingleBrick(
-          mockBrick,
-          analytics: mockAnalytics,
-          logger: mockLogger,
-          fileSystem: memoryFileSystem,
-        );
+      group('gracefully', () {
+        test('when shouldSync and isWatch are default values', () async {
+          final runner = CookSingleBrick(
+            mockBrick,
+            analytics: mockAnalytics,
+            logger: mockLogger,
+            fileSystem: memoryFileSystem,
+          );
 
-        final result = await runner.run();
+          final result = await runner.run();
 
-        verify(mockLogger.preheat).called(1);
-        verify(mockBrick.cook).called(1);
-        verify(mockLogger.dingDing).called(1);
+          verify(mockBrick.cook).called(1);
 
-        verify(
-          () => mockAnalytics.sendEvent(
-            'cook',
-            'one',
-            label: 'no-watch',
-            value: 0,
-            parameters: {
-              'bricks': '1',
-              'sync': 'true',
-            },
-          ),
-        ).called(1);
+          verify(mockLogger.preheat).called(1);
+          verify(mockLogger.dingDing).called(1);
 
-        verify(
-          () => mockAnalytics.waitForLastPing(
-            timeout: any(named: 'timeout'),
-          ),
-        ).called(1);
+          verify(
+            () => mockAnalytics.sendEvent(
+              'cook',
+              'one',
+              label: 'no-watch',
+              value: 0,
+              parameters: {
+                'bricks': '1',
+                'sync': 'true',
+              },
+            ),
+          ).called(1);
 
-        expect(result, ExitCode.success.code);
+          verify(
+            () => mockAnalytics.waitForLastPing(
+              timeout: any(named: 'timeout'),
+            ),
+          ).called(1);
+
+          verifyNoMoreInteractions(mockAnalytics);
+          verifyNoMoreInteractions(mockLogger);
+          verifyNoMoreInteractions(mockBrick);
+
+          expect(result, ExitCode.success.code);
+        });
+
+        group('when shouldSync', () {
+          test('is false', () async {
+            final runner = TestCookSingleBrick(
+              mockBrick,
+              analytics: mockAnalytics,
+              logger: mockLogger,
+              fileSystem: memoryFileSystem,
+              shouldSync: false,
+            );
+
+            final result = await runner.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: false,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'one',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'false',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockAnalytics);
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+
+            expect(result, ExitCode.success.code);
+          });
+
+          test('is true', () async {
+            final runner = TestCookSingleBrick(
+              mockBrick,
+              analytics: mockAnalytics,
+              logger: mockLogger,
+              fileSystem: memoryFileSystem,
+              shouldSync: true,
+            );
+
+            final result = await runner.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'one',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockAnalytics);
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+
+            expect(result, ExitCode.success.code);
+          });
+        });
+
+        group('when isWatch', () {
+          late Completer<int> exitCompleter;
+          late KeyPressListener keyPressListener;
+
+          setUp(() {
+            exitCompleter = Completer<int>();
+
+            final mockStdin = MockStdin();
+
+            when(() => mockStdin.hasTerminal).thenReturn(true);
+
+            when(mockStdin.asBroadcastStream).thenAnswer(
+              (_) => Stream.fromIterable([
+                'q'.codeUnits,
+              ]),
+            );
+
+            keyPressListener = KeyPressListener(
+              stdin: mockStdin,
+              logger: mockLogger,
+              toExit: exitCompleter.complete,
+            );
+
+            final mockSource = MockSource();
+            final mockWatcher = MockWatcher();
+
+            when(() => mockBrick.source).thenReturn(mockSource);
+            when(() => mockSource.watcher).thenReturn(mockWatcher);
+
+            when(() => mockWatcher.hasRun).thenAnswer((_) => true);
+            when(mockWatcher.start).thenAnswer((_) => Future.value());
+            when(mockWatcher.stop).thenAnswer((_) => Future.value());
+          });
+
+          tearDown(() {
+            KeyPressListener.stream = null;
+          });
+
+          test('is false', () async {
+            final runner = TestCookSingleBrick(
+              mockBrick,
+              analytics: mockAnalytics,
+              logger: mockLogger,
+              fileSystem: memoryFileSystem,
+              isWatch: false,
+              keyPressListener: keyPressListener,
+            );
+
+            final result = await runner.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: false,
+              ),
+            ).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'one',
+                label: 'no-watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockAnalytics);
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+
+            expect(result, ExitCode.success.code);
+          });
+
+          test('is true', () async {
+            final runner = TestCookSingleBrick(
+              mockBrick,
+              analytics: mockAnalytics,
+              logger: mockLogger,
+              fileSystem: memoryFileSystem,
+              isWatch: true,
+              keyPressListener: keyPressListener,
+            );
+
+            final runResult = await runner.run();
+
+            verify(
+              () => mockBrick.cook(
+                output: any(named: 'output'),
+                sync: true,
+                watch: true,
+              ),
+            ).called(1);
+            verify(() => mockBrick.configPath).called(1);
+            verify(() => mockBrick.source).called(1);
+
+            verify(mockLogger.preheat).called(1);
+            verify(mockLogger.dingDing).called(1);
+            verify(mockLogger.watching).called(1);
+            verify(mockLogger.quit).called(1);
+            verify(mockLogger.reload).called(1);
+            verify(mockLogger.exiting).called(1);
+
+            verify(
+              () => mockAnalytics.sendEvent(
+                'cook',
+                'one',
+                label: 'watch',
+                value: 0,
+                parameters: {
+                  'bricks': '1',
+                  'sync': 'true',
+                },
+              ),
+            ).called(1);
+
+            verify(
+              () => mockAnalytics.waitForLastPing(
+                timeout: any(named: 'timeout'),
+              ),
+            ).called(1);
+
+            verifyNoMoreInteractions(mockAnalytics);
+            verifyNoMoreInteractions(mockLogger);
+            verifyNoMoreInteractions(mockBrick);
+
+            final result = await exitCompleter.future;
+
+            expect(result, ExitCode.success.code);
+            expect(runResult, ExitCode.success.code);
+          });
+        });
       });
 
-      test('logs when unknown error occurs', () async {
+      test('when unknown error occurs', () async {
         when(mockBrick.cook).thenThrow(Exception('error'));
         when(() => mockBrick.name).thenReturn('BRICK');
 
@@ -170,7 +425,7 @@ void main() {
         expect(result, ExitCode.success.code);
       });
 
-      test('logs when config error occurs', () async {
+      test('when config error occurs', () async {
         when(mockBrick.cook)
             .thenThrow(const BrickException(brick: 'BRICK', reason: 'error'));
         when(() => mockBrick.name).thenReturn('BRICK');
@@ -216,4 +471,41 @@ void main() {
       });
     });
   });
+}
+
+class TestCookSingleBrick extends CookSingleBrick {
+  TestCookSingleBrick(
+    Brick brick, {
+    required FileSystem fileSystem,
+    required Analytics analytics,
+    required Logger logger,
+    bool? isWatch,
+    bool? shouldSync,
+    KeyPressListener? keyPressListener,
+  })  : _isWatch = isWatch,
+        _shouldSync = shouldSync,
+        super(
+          brick,
+          fileSystem: fileSystem,
+          analytics: analytics,
+          logger: logger,
+          keyPressListener: keyPressListener,
+        );
+
+  final bool? _isWatch;
+  final bool? _shouldSync;
+
+  @override
+  bool get isWatch => _isWatch ?? super.isWatch;
+
+  @override
+  bool get shouldSync => _shouldSync ?? super.shouldSync;
+
+  @override
+  Future<bool> watchForConfigChanges(
+    String _, {
+    FutureOr<void> Function()? onChange,
+  }) async {
+    return true;
+  }
 }
