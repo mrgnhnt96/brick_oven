dc22c5a2a2643158982ff854b078be6d6d093f55
Morgan <mrgnhnt96@gmail.com>
2022-11-22 14:49:20 -0600

    feat: create constants file with reused values

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+/// the value to be used for when accessing the current value of an array
+const String kIndexValue = '_INDEX_VALUE_';
+
+/// the default number of braces to wrap a variable with
+const int kDefaultBraces = 3;
---
+++ test/utils/constants_test.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+import 'package:brick_oven/utils/constants.dart';
+
+void main() {
+  group('constants', () {
+    test('kIndexValue is correct value', () {
+      expect(kIndexValue, '_INDEX_VALUE_');
+    });
+
+    test('kDefaultBraces is correct value', () {
+      expect(kDefaultBraces, 3);
+    });
+  });
+}
