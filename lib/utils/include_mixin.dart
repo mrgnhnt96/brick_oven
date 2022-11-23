7ca3c90ff84a6b1f9018473ebeb08b3c37b41a1c
Morgan <mrgnhnt96@gmail.com>
2022-11-23 16:56:49 -0600

    feat: create include mixin for reusability

---
+++ lib/utils/include_mixin.dart
@@ -0,0 +1,32 @@
+import 'package:brick_oven/domain/yaml_value.dart';
+import 'package:brick_oven/src/exception.dart';
+
+/// a mixin for [includeIf] and [includeIfNot]
+abstract class IncludeMixin {
+  /// whether to include the file in the _mason_ build output
+  /// based on the variable provided
+  ///
+  /// wraps the file in a `{{^if}}` block
+  String? get includeIf;
+
+  /// whether to include the file in the _mason_ build output
+  /// based on the variable provided
+  ///
+  /// wraps the file in a `{{^if}}` block
+  String? get includeIfNot;
+
+  /// retrieves [includeIf] or [includeIfNot] from [yaml]
+  static String? getInclude(YamlValue yaml, String type) {
+    if (yaml.isNone()) {
+      return null;
+    }
+
+    if (!yaml.isString()) {
+      throw BrickConfigException(
+        reason: 'Expected type `String` or `null` for `$type`',
+      );
+    }
+
+    return yaml.asString().value;
+  }
+}
