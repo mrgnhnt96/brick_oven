b0be0040efa9e4acac1fe982cbfa1b379a5db8c3
Morgan <mrgnhnt96@gmail.com>
2022-11-23 16:08:19 -0600

    feat: create test for e2e

--- test/e2e/e2e_test.dart
+++ test/e2e/e2e_test.dart
@@ -8,20 +8,21 @@ import 'util/cook_brick.dart';
 void main() {
   group('cook runs gracefully', () {
     const bricks = {
-      'bio': 1,
-      'documentation': 4,
-      'favorite_color': 1,
-      'flavors': 2,
-      'greeting': 1,
-      'hello': 1,
-      'hello_world': 3,
-      'hooks': 1,
-      'legacy': 1,
-      'photos': 1,
-      'plugin': 9,
-      'random_color': 1,
-      'simple': 1,
-      'todos': 3,
+      'app_icon': 1,
+      // 'bio': 1,
+      // 'documentation': 4,
+      // 'favorite_color': 1,
+      // 'flavors': 2,
+      // 'greeting': 1,
+      // 'hello': 1,
+      // 'hello_world': 3,
+      // 'hooks': 1,
+      // 'legacy': 1,
+      // 'photos': 1,
+      // 'plugin': 9,
+      // 'random_color': 1,
+      // 'simple': 1,
+      // 'todos': 3,
     };
 
     for (final brick in bricks.entries) {
