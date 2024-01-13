1fa135bf800b6ab67f2944e4dc16bd668a3b6f2f
Morgan <mrgnhnt96@gmail.com>
2024-01-13 14:06:31 -0700

    feat: safely get arguments even if they aren't there

--- lib/utils/brick_cooker.dart
+++ lib/utils/brick_cooker.dart
@@ -27,11 +27,19 @@ mixin BrickCookerArgs on Command<int> {
   /// whether to listen to changes to the
   /// [BrickOvenYaml.file] or the [BrickConfig.configPath] file and
   /// the [BrickConfig.sourcePath] directory
-  bool get isWatch => argResults?['watch'] as bool? ?? false;
+  bool get isWatch => _get<bool>('watch') ?? false;
 
   /// whether to validate if the brick.yaml file is synced
-  bool get shouldSync => argResults?['sync'] as bool? ?? true;
+  bool get shouldSync => _get<bool>('sync') ?? true;
 
   /// the directory to output the brick to
-  String? get outputDir => argResults?['output'] as String?;
+  String? get outputDir => _get<String>('output');
+
+  T? _get<T>(String key) {
+    try {
+      return argResults?[key] as T?;
+    } catch (_) {
+      return null;
+    }
+  }
 }
